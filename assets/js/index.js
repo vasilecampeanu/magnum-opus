53062dacf739b069d5d3369dd4e211bbdec49664
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-06-04 17:08:08 +0300

    feat[partial]: Hide navigation bar when scrolling down and show it when scrolling up.

--- assets/js/index.js
+++ assets/js/index.js
@@ -66,6 +66,34 @@ function ThemeSwitcher(params) {
     });
 }
 
+function HideNavbar() {
+    const body = document.body;
+    let lastScroll = 20;
+    
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.pageYOffset;
+    
+        if (currentScroll <= 20)
+        {
+            body.classList.remove("scroll-up");
+            return;
+        }
+    
+        if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) 
+        {
+            body.classList.remove("scroll-up");
+            body.classList.add("scroll-down");
+        } 
+        else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) 
+        {
+            body.classList.remove("scroll-down");
+            body.classList.add("scroll-up");
+        }
+    
+        lastScroll = currentScroll;
+    });
+}
+
 $(document).ready(function () {
     // Preload
     pageLoad();
@@ -76,4 +104,8 @@ $(document).ready(function () {
 
     // Theme
     ThemeSwitcher();
+
+    // Navigation bar
+    HideNavbar();
 });
+
